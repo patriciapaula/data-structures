a368a9667879dc73efa8d05b1f2bfed64e415fbb
patriciapaula <patricia.paula@gmail.com>
2022-09-06 18:41:11 -0300

    adding blank test

---
+++ list/list_test.go
@@ -0,0 +1,12 @@
+package list
+
+import "testing"
+
+func TestListInsere(t *testing.T) {
+	test := []int{1, 2, 3, 4}
+	/* total := lis(test...)
+
+	if total != 10 {
+		t.Fatalf("Valor esperado: 10 - Valor retornado: %d", total)
+	} */
+}
